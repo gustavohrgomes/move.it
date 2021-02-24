434a0a55d5f4090555339ecc333041bfdad89aef
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-02-23 22:30:16 -0300

    Finalizando a primeira etapada da aplicação

---
+++ src/components/Profile.tsx
@@ -0,0 +1,19 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img
+        src="https://avatars.githubusercontent.com/u/43626534?s=460&u=56e15e81733779dd5ba5adb80c283d9b55a2a4fd&v=4"
+        alt="Gustavo Gomes"
+      />
+      <div>
+        <strong>Gustavo Gomes</strong>
+        <p>
+          <img src="icons/level.svg" alt="level up" />
+          Level 1
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,28 @@
+import Head from 'next/head';
+import { CompletedChallengeds } from '../components/CompletedChallenges';
+import { Countdown } from '../components/Countdown';
+import { ExperienceBar } from '../components/ExperienceBar';
+import { Profile } from '../components/Profile';
+
+import Styles from '../styles/pages/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={Styles.container}>
+      <Head>
+        <title>Inicio | move.it</title>
+      </Head>
+
+      <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallengeds />
+          <Countdown />
+        </div>
+        <div></div>
+      </section>
+    </div>
+  );
+}
